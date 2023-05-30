d6b22fc3f0d01b037df62892bab0803819765185
Ozy9665 <ozy9665@gmail.com>
2023-05-30 18:33:03 +0900

    지역 리스트 삽입

--- karaokeNrestaurant.py
+++ karaokeNrestaurant.py
@@ -1,10 +1,14 @@
 import requests
 import xml.etree.ElementTree as ET
 from tkinter import *
+from tkinter import font
 import tkinter.ttk
+import Sggucd
 
 
-#
+
+
+
 #노래방 서비스 예제
 karaoke_url = 'https://openapi.gg.go.kr/sngrumIndutype?'
 restaurant_url = 'https://openapi.gg.go.kr/GENRESTRT?'
@@ -22,8 +26,30 @@ restaurant_response = requests.get(restaurant_url, params=restaurant_queryParams
 print(restaurant_response.text)
 restaurant_root = ET.fromstring(restaurant_response.text)
 
-window = tkinter.Tk()
+window = Tk()
 window.title("노래방,식당")
+DataList = []
+
+# 검색기능
+def InitSearchBox():
+    global SearchBox
+    ListBoxScrollbar = Scrollbar(window)
+    ListBoxScrollbar.pack()
+    ListBoxScrollbar.place(x=150, y=50)
+
+    TempFont = font.Font(window, size=15, weight='bold', family='Consolas')
+    SearchListBox = Listbox(window, font=TempFont, activestyle='none',
+                            width=10, height=5, borderwidth=12, relief='ridge',
+                            yscrollcommand=ListBoxScrollbar.set)
+
+    for i in range(len(Sggucd.SGGUCD)):
+        SearchListBox.insert(i+1, Sggucd.SGGUCD[i][1])  # 지역 리스트 삽입
+
+    SearchListBox.pack()
+    SearchListBox.place(x=10, y=50)
+
+    ListBoxScrollbar.config(command=SearchListBox.yview)
+
 
 # 페이지 나누기
 notebook = tkinter.ttk.Notebook(window, width=800, height=600)
@@ -37,6 +63,7 @@ header = ["Name", "Addr", "TELNO"]
 res_header = ["ResName", "ResAddr", "ResTel"]
 
 # 노래방
+'''
 for i, col_name in enumerate(header):
     label = Label(frame1, text=col_name, font=("Helvetica", 14, "bold"))
     label.grid(row=0, column=i)
@@ -54,12 +81,13 @@ for item in karaoke_root.iter("row"):
         label.grid(row=row_count, column=i)
 
     row_count += 1
-
+'''
 
 frame2=Frame(window)
 notebook.add(frame2, text='식당')
 Label(frame2, text='페이지2내용', fg='blue', font='helvetica 48')
 # 레스토랑
+'''
 for i, col_name in enumerate(res_header):
     label = tkinter.Label(frame2, text=col_name, font=("Helvetica", 14, "bold"))
     label.grid(row=row_count, column=i)
@@ -77,6 +105,7 @@ for item in restaurant_root.iter("row"):
         label.grid(row=row_count, column=i)
 
     row_count += 1
-
+'''
+InitSearchBox()
 window.mainloop()
 
